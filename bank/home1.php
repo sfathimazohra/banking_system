1a981704c31b919c58b8a255cc50b1cb06681858
sfathimazohra <53909966+sfathimazohra@users.noreply.github.com>
2021-05-20 00:12:42 +0530

    Add files via upload

---
+++ bank/home1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Taxation Bank</title>
+    <link rel="stylesheet" href="style1.css">
+  </head>
+
+  <body style="background: rgb(238,174,202);
+background: linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(214,178,210,1) 12%, rgba(148,187,233,1) 100%);">
+
+    <ul>
+  <li><a class="active" href="home2.php">HOME</a></li>
+  <li><a href="transfer.php">TRANSFER MONEY</a></li>
+  <li><a href="transfer.php">VIEW CUSTOMERS</a></li>
+  <li><a href="transactionhistory.php">TRANSACTION HISTORY</a></li>
+</ul>
+<p class="welcome" style="text-align:center;
+font-size: 50px;
+float:center;
+">
+WELCOME TO TAXATION BANK.<br>
+YOUR MONEY.<br>
+OUR RESPONSIBILITY.</p>
+
+  </body>
+</html>
---
+++ bank/naav.php
@@ -0,0 +1,22 @@
+<!-- navbar -->
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : pink;"><b>TAXATION BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="home1.php" style="color :black;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfer.php" style="color : black"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfer.php" style="color : black"><b>View Customers</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : black;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
